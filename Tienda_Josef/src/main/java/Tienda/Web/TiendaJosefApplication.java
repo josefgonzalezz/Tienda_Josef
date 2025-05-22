d16c1795871d340f2edbc83b25fcc645a1a9a5f5
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2025-05-21 19:15:27 -0600

    Mi primer commit
    Mi primer commit

---
+++ Tienda_Josef/src/main/java/Tienda/Web/TiendaJosefApplication.java
@@ -0,0 +1,13 @@
+package Tienda.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJosefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJosefApplication.class, args);
+	}
+
+}
