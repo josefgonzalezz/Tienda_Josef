ca3695f455a8316673a397a8591f8f97368add57
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2025-06-18 20:28:35 -0600

    Semana 5 y 6
    Trabajo en clase de la semana 5 y 6

---
+++ Tienda_Josef/src/main/java/Tienda/Web/dao/CategoriaDao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package Tienda.Web.dao;
+
+/**
+ *
+ * @author josef
+ */
+
+import Tienda.Web.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaDao extends JpaRepository <Categoria,Long> {
+    
+}
+ 
